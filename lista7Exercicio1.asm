07316deac499717ab5ae1225cb059438f664e236
felipe based mito <42080804+felipeyousoro@users.noreply.github.com>
2022-05-11 18:24:11 -0300

    Add files via upload

--- lista7Exercicio1.asm
+++ lista7Exercicio1.asm
@@ -84,21 +84,6 @@
 			move $v0, $t0
 .end_macro
 
-.macro strlenData(%data)
-	.text
-		main:
-			li $t0, 0
-			move $t1, %data
-		loop:
-			lb $t2, ($t1)
-			beqz $t2, end
-			add $t1, $t1, 1
-			add $t0, $t0, 1
-			j loop
-		end:
-			move $v0, $t0
-.end_macro
-
 .macro writeFileString(%file, %string)
 	.data
 		string: .asciiz %string
@@ -115,7 +100,7 @@
 	.text
 		move $a0, %file
 		move $a1, %data
-		strlenData(%data)
+		strlenAddress(%data)
 		move $a2, $v0
 		li $v0, 15
 		syscall
@@ -128,6 +113,71 @@
 		syscall
 .end_macro
 
+.macro strlenAddress(%string)
+	.text
+		main:
+			li $t0, 0
+			move $t1 %string
+		loop:
+			lb $t2, ($t1)
+			beqz $t2, end
+			add $t1, $t1, 1
+			add $t0, $t0, 1
+			j loop
+		end:
+			move $v0, $t0
+.end_macro
+
+.macro invertStringAddress(%string)
+	.text
+		main:
+			move $a3 $v0
+			strlenAddress(%string)
+			move $v1 $v0
+			move $v0 $a3
+			sub $v1 $v1 1
+			li $a0 0
+			blt $v1 1 end
+		loop:
+			add $a1 $a0 %string
+			lb $t0 ($a1)
+			add $a2 $v1 %string
+			lb $t1 ($a2)
+			sb $t1 ($a1)
+			sb $t0 ($a2)
+			add $a0 $a0 1
+			sub $v1 $v1 1
+			bgt $v1 $a0 loop 				
+		end:
+.end_macro
+
+.macro intToString(%int)
+	.data 
+		string: .word 32
+	.text
+		main:
+			la $v0 string
+			li $a0 10
+			move $a1 %int
+			blt $a1 $a0 end
+		loop:
+			div $a1 $a0
+			mflo $a1
+			mfhi $a2
+			add $a2 $a2 48
+			sb $a2 ($v0)
+			add $v0 $v0 1
+			bge $a1 $a0 loop # maior ou igual a 10
+		end:
+			add $a1 $a1 48
+			sb $a1 ($v0)
+			la $v0 string
+			strlenAddress($v0)
+			la $v0 string
+			invertStringAddress($v0)
+			la $v0 string
+.end_macro
+
 .text
 	main:
 		readInt()
@@ -136,6 +186,7 @@
 		alloc($t0)
 		move $s1 $v0
 		jal fillWithPrimes
+		jal writeTwins
 		end()
 			
 	fillWithPrimes:
@@ -167,11 +218,11 @@
 			lw $t2 ($t5)
 			lw $t3 4($t5)
 			sub $t3 $t3 $t2
-			bgt $t3 2 writeTwinsNext
-			add $t3 $t3 48
-			sb $t3 ($t9)
-			writeFileData($t9 $t8)
-			writeFileString($t9 " ")
+			bne $t3 2 writeTwinsNext
+			intToString($t2)
+			move $s5 $v0
+			writeFileData($t8, $s5)
+			writeFileString($t8, " ")
 				writeTwinsNext:
 			add $t5 $t5 4
 			add $t7 $t7 1
